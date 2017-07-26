8b5bf2c866eb1d69ce4d4a9ee7f9f5b24312cc65
René Perschon <rperschon85@gmail.com>
2017-07-26 22:10:55 +0200

    added capacity param validations

--- lib/scooters.rb
+++ lib/scooters.rb
@@ -13,6 +13,9 @@ module Scooters
     raise ArgumentError.new("cap_engineers is required") unless opts[:cap_engineers]
     raise ArgumentError.new("scooters is required") unless opts[:scooters]
 
+    raise ArgumentError.new("cap_manager must be at least 0") if opts[:cap_manager] < 0
+    raise ArgumentError.new("cap_engineers must be at least 0") if opts[:cap_engineers] < 0
+
     42
   end
 
--- spec/scooters_spec.rb
+++ spec/scooters_spec.rb
@@ -1,25 +1,49 @@
 require "spec_helper"
 
 RSpec.describe Scooters do
+  DEFAULT = {
+    cap_manager: 1,
+    cap_engineers: 1,
+    scooters: [1]
+  }
+
+  def DEFAULT.except(*exceptions)
+    self.clone.delete_if {|k, _| exceptions.include? k }
+  end
+
+  def DEFAULT.with(hash)
+    self.clone.merge(hash)
+  end
+
   it "has a version number" do
     expect(Scooters::VERSION).not_to be nil
   end
 
   it "requires all three parameters" do
     expect {
-      Scooters.min_required_engineers(cap_engineers: 1, scooters: [1])
+      Scooters.min_required_engineers(DEFAULT.except :cap_manager)
     }.to raise_exception(ArgumentError)
 
     expect {
-      Scooters.min_required_engineers(cap_manager: 1, scooters: [1])
+      Scooters.min_required_engineers(DEFAULT.except :cap_engineers)
     }.to raise_exception(ArgumentError)
 
     expect {
-      Scooters.min_required_engineers(cap_manager: 1, cap_engineers: 1)
+      Scooters.min_required_engineers(DEFAULT.except :scooters)
     }.to raise_exception(ArgumentError)
 
     expect {
-      Scooters.min_required_engineers(cap_manager: 1, cap_engineers: 1, scooters: [1])
+      Scooters.min_required_engineers(DEFAULT)
     }.not_to raise_exception
+  end
+
+  it "validates cap parameters" do
+    expect {
+      Scooters.min_required_engineers(DEFAULT.with cap_manager: -1)
+    }.to raise_exception(ArgumentError)
+
+    expect {
+      Scooters.min_required_engineers(DEFAULT.with cap_engineers: -1)
+    }.to raise_exception(ArgumentError)
   end
 end
