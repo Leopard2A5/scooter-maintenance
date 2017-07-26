ce6b82701dd1c7a8f294cd417e72961f6f6c4361
René Perschon <rperschon85@gmail.com>
2017-07-26 21:48:50 +0200

    added some basic param validation

--- lib/scooters.rb
+++ lib/scooters.rb
@@ -2,7 +2,17 @@ require "scooters/version"
 
 module Scooters
 
-  def self.min_required_engineers(*args)
+  def self.min_required_engineers(opts = {})
+    opts = {
+      cap_manager: nil,
+      cap_engineers: nil,
+      scooters: nil
+    }.merge(opts)
+
+    raise ArgumentError.new("cap_manager is required") unless opts[:cap_manager]
+    raise ArgumentError.new("cap_engineers is required") unless opts[:cap_engineers]
+    raise ArgumentError.new("scooters is required") unless opts[:scooters]
+
     42
   end
 
--- spec/scooters_spec.rb
+++ spec/scooters_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Scooters do
     expect(Scooters::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    # expect(false).to eq(true)
+  it "requires all three parameters" do
+    expect {
+      Scooters.min_required_engineers(cap_engineers: 1, scooters: [1])
+    }.to raise_exception(ArgumentError)
+
+    expect {
+      Scooters.min_required_engineers(cap_manager: 1, scooters: [1])
+    }.to raise_exception(ArgumentError)
+
+    expect {
+      Scooters.min_required_engineers(cap_manager: 1, cap_engineers: 1)
+    }.to raise_exception(ArgumentError)
+
+    expect {
+      Scooters.min_required_engineers(cap_manager: 1, cap_engineers: 1, scooters: [1])
+    }.not_to raise_exception
   end
 end
